6a2c06699c1293513dfe3e0dde96ac43dce4e865
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-12 13:54:49 -0400

    Drawer setup

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    )
+    ),
   );
 }
 
@@ -22,12 +22,90 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
         title: Text('Easy Sales'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: (){}),
-          IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed: (){}),
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Didier Ganthier'),
+              accountEmail: Text('didierganthierperan@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.redAccent),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My orders'),
+                leading: Icon(Icons.shopping_basket, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Favorites'),
+                leading: Icon(Icons.favorite, color: Colors.pink),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help, color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
