8519b009710320a4f5f71638b2a0b2caa29252fa
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-13 17:22:37 -0400

    Carousel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,6 +18,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget image_carousel = Container(
+      height: 200,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/IMG_1266.JPG'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+        animationDuration: Duration(seconds: 1),
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         elevation: 0.1,
